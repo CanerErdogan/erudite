8cbfd7e5f484c9f913b2d56c61c154241cf22116
Caner <caner.erdogan.92@gmail.com>
2020-11-09 13:51:43 +0300

    Background refresh fixed and blur implemented

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 const ERUDITE_API_PORT = 3001;
 
-const API_URL = `http://localhost:${ERUDITE_API_PORT}`
+const API_URL = `http://localhost:${ERUDITE_API_PORT}`;
+const DEFAULT_BG_URL = "https://images.unsplash.com/photo-1589366025085-d060e3f956e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80";
 
-module.exports = { API_URL };
+module.exports = { API_URL, DEFAULT_BG_URL };
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,24 +5,40 @@ import './App.css';
 
 import NoteCard from '../components/NoteCard/NoteCard';
 import SearchBar from '../components/SearchBar/SearchBar';
-import { API_URL } from '../constants';
+import { API_URL, DEFAULT_BG_URL } from '../constants';
 
 function App() {
   const [background, setBackground] = useState("unset");
+  const [cssProps, setCssProps] = useState({});
 
   const changeBackground = () => {
     fetch(`${API_URL}/background`)
       .then(res => res.json()).then(data => {
         console.log(data);
-        setBackground(`url("${data.urls.regular}")`);
+        setBackground(`url(${data.urls.regular})`);
       })
-      .catch(err => { console.log("App fail: " + err) })
-      .catch(err => { setBackground('url("https://images.unsplash.com/photo-1589366025085-d060e3f956e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80")') });
+      .catch(err => { 
+        console.log("App fail: " + err);
+        setBackground(`url(${DEFAULT_BG_URL})`);
+        // setCssProps({'--background': 'url("https://images.unsplash.com/photo-1589366025085-d060e3f956e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80")'});
+      });
+    
   };
 
   useEffect(() => { changeBackground() }, []);
+  useEffect(() => { setCssProps({'--background': background}); }, [background]);
 
   return (
+    <div id="variables" style={cssProps}>
+      <div className="bg-wrapper">
+        <div className="App">
+          <NoteCard />
+          <SearchBar />
+          <button className="f6 link dim br1 ba ph3 pv2 mb2 dib black pointer" onClick={changeBackground}>Change Background</button>
+        </div>
+      </div>
+    </div>
+  );/* (
     <div className="App">
       <div style={{
         backgroundImage: `${background}`,
@@ -36,7 +52,7 @@ function App() {
         <button className="f6 link dim br1 ba ph3 pv2 mb2 dib black pointer" onClick={changeBackground}>Change Background</button>
       </div>
     </div>
-  );
+  ); */
 }
 
 export default App;
